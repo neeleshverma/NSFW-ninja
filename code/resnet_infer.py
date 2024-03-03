548fe2a7463c3be02c7494c0ddd3694ff9d60a4e
Neelesh Verma <neeleshverma13@gmail.com>
2024-03-03 18:00:15 -0500

    Modified training inferece for 2 outputs

--- code/resnet_infer.py
+++ code/resnet_infer.py
@@ -8,14 +8,11 @@ import torch.utils.data
 from torch import nn
 import torchvision
 from torchvision import transforms
-from torchvision.models import ResNet18_Weights, ResNet34_Weights, ResNet50_Weights, ResNet101_Weights
 import torch.nn.functional as F
 
 import os
 
 devices = None
-
-resnet_weights = {'resnet18':ResNet18_Weights, 'resnet34':ResNet34_Weights, 'resnet50':ResNet50_Weights, 'resnet101':ResNet101_Weights}
 
 def test(args):
     test_dataset = classifier_utils.loadDataset(datadir = args['test_dir'], train=False)
@@ -24,9 +21,9 @@ def test(args):
         shuffle=False, num_workers=args['num_workers'], pin_memory=args['pin_memory'])
 
     # Architecture Loading
-    model = torchvision.models.__dict__[args['model']](weights=resnet_weights[args['model']].IMAGENET1K_V1)
+    model = torchvision.models.__dict__[args['model']](weights=None)
     num_ftrs = model.fc.in_features
-    model.fc = nn.Linear(num_ftrs, 1)
+    model.fc = nn.Linear(num_ftrs, 2)
     model.to(devices[0])
     model = nn.DataParallel(model, args['gpus'])
 
@@ -42,20 +39,17 @@ def test(args):
 
     correct_preds = 0
     test_loss = 0.0
-    criterion = nn.BCEWithLogitsLoss()
+    criterion = nn.CrossEntropyLoss()
 
     with torch.no_grad():
         for i, (image, target) in enumerate(test_data_loader):
             image, target = image.to(devices[0]), target.to(devices[0])
-            target = target.type(torch.float)
             
             output = model(image)
-            output = output.squeeze()
-
             loss = criterion(output, target)
 
             test_loss += loss.item()
-            y_pred = torch.round(torch.sigmoid(output))
+            _, y_pred = torch.max(output, dim=1)
             correct_preds += torch.sum(y_pred == target).cpu()
     
     acc = correct_preds / len(test_data_loader.dataset)
